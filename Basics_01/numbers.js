a2762bbcf0b98092a7b669803d72dc751b23befb
Manabendu-ai <rikugithub@gmail.com>
2026-04-03 23:26:26 +0530

    Numbers

---
+++ Basics_01/numbers.js
@@ -0,0 +1,22 @@
+const score = 400
+
+let bal = new Number(100)
+console.log(bal)
+
+console.log(bal.toString().length)
+console.log(bal.toFixed(2))
+
+const bmi = 23.8773
+console.log(bmi.toPrecision(3))
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN')) // 1,00,00,000
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.EPSILON)
+
+console.log(Math.abs(-77))
+console.log(Math.round(5.76))
+console.log(Math.floor(4.88)) 
+console.log(Math.ceil(4.13))
