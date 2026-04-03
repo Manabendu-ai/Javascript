f09bb879fe96c4e129d1000661e8325a34e680d2
Manabendu-ai <rikugithub@gmail.com>
2026-04-03 16:17:42 +0530

    Conversion

---
+++ Basics_01/conversions.js
@@ -0,0 +1,19 @@
+let score = "77abc" // String
+
+console.log(typeof score);
+
+let intScore = Number(score) // String to Number convertion
+
+console.log(typeof intScore);
+
+console.log(intScore); // -> is 77abc is not a Number it will give NaN so we must do conversions very carefully!
+
+/*
+
+    Converting String Integer to Int -> Number
+    Converting String to Int -> NaN
+    Converting null to Int -> 0
+    Converting undefined to Int -> NaN
+    Converting boolean to Int -> 1 : true or 0 : false
+
+ */
