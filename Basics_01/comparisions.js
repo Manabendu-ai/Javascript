9fcdc1990f822dc7cf319169a986c50055eaa6b8
Manabendu-ai <rikugithub@gmail.com>
2026-04-03 17:04:31 +0530

    comparisons

---
+++ Basics_01/comparisions.js
@@ -0,0 +1,29 @@
+// console.log(11>4)
+// console.log(91<=56)
+
+// console.log("2"==2)
+// console.log("02">4);
+
+console.log(null == 0) // false
+console.log(null > 0) // false
+console.log(null < 0) // false
+console.log(null >= 0) // true
+console.log(null <= 0) // true
+
+// same with undefined as well
+
+console.log(null == undefined) // true
+
+/*
+
+    null >= 0  → !(null < 0)
+    null <= 0  → !(null > 0)
+
+*/
+
+// === -> Strictly compares both the values as well as the data types
+// == -> Compares only the values not the data types
+
+console.log("2" == 2)
+console.log("2" === 2)
+
