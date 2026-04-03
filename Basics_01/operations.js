98af1af332896ebee167d95ed49c3cf32863a621
Manabendu-ai <rikugithub@gmail.com>
2026-04-03 16:50:26 +0530

    Operations

---
+++ Basics_01/operations.js
@@ -0,0 +1,39 @@
+let value = 3
+let negValue = -value;
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(3-5);
+// console.log(3**2);
+// console.log(5/3);
+// console.log(6%3);
+
+
+let str1 = "Manabendu "
+let str2 = "Karfa!"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("9"+2);
+console.log("1"+4+4);
+console.log(1+4+"4");
+
+
+console.log(+true) // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 5 + 6 // Not a good way to write code bcoz not readle
+
+let gameCounter = 15
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
